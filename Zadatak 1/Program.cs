174858852737c99db27dd981b0dd3122df44b447
ClashMania <113007885+ClashMania@users.noreply.github.com>
2023-01-25 09:07:58 +0100

    Add files via upload

---
+++ Zadatak 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Napišite program u kojem ćete zatražiti od korisnika da unese niz znakova te će svaki razmak zamijeniti znakom '_'.
+namespace Zadatak_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string niz;
+            Console.WriteLine("Upišite niz znakova: ");
+            niz = Console.ReadLine();
+            Console.WriteLine(niz.Replace(" ", "_"));
+
+            Console.ReadKey();
+        
+
+    }
+    }
+}
